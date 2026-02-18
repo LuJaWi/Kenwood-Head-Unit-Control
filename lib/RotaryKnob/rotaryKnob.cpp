00c243842d0d50d810e3a2de84e1016088fdea74
Luke Witt <l.witt@ansabio.com>
2026-02-17 16:15:14 -0800

    putting debounce before state check

--- lib/RotaryKnob/rotaryKnob.cpp
+++ lib/RotaryKnob/rotaryKnob.cpp
@@ -6,18 +6,18 @@ RotaryKnob::RotaryKnob(int clk_pin, int dt_pin, int sw_pin) : clk_pin(clk_pin), 
     pinMode(dt_pin, INPUT);
     pinMode(sw_pin, INPUT_PULLUP);
     clk_last_state = digitalRead(clk_pin);
-} 
+}
 
 int RotaryKnob::checkRotation(){
     unsigned long current_time = millis();
-    int current_state = digitalRead(clk_pin); // Reads the "current" state of the outputA
     int rotation = 0;
-    
-    // If the previous and the current state of the outputA are different, that means a Pulse has occured
-    if (current_state != clk_last_state)
+
+    // Check if enough time has passed since the last change (debounce)
+    if (current_time - last_change_time > debounce_delay)
     {
-        // Check if enough time has passed since the last change (debounce)
-        if (current_time - last_change_time > debounce_delay)
+        // If the previous and the current state of the outputA are different, that means a Pulse has occured
+        int current_state = digitalRead(clk_pin); // Reads the "current" state of the outputA
+        if (current_state != clk_last_state)
         {
             // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
             if (digitalRead(dt_pin) != current_state)
@@ -28,13 +28,13 @@ int RotaryKnob::checkRotation(){
             {
                 rotation = -1;
             }
-            
+
             // Check for direction change bounce BEFORE updating timestamps
             unsigned long time_since_last_rotation = current_time - last_change_time;
             bool is_direction_change = (last_rotation != rotation && last_rotation != 0);
-            
 
-            
+
+
             // Filter out direction changes that happen too quickly (likely bounce)
             if (is_direction_change && time_since_last_rotation < direction_change_delay) {
                 return 0;
